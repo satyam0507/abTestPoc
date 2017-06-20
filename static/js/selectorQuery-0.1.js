36b7c8ec50a8ded19d8bdd049e9e5c66664e7f87
satyam0507@gmail.com <satyam0507@gmail.com>
2017-06-20 18:59:42 +0530

    added static/js/selectorQuery-0.1.js

---
+++ static/js/selectorQuery-0.1.js
@@ -0,0 +1,68 @@
+function selectorQuery(node) {
+    var ignoreClassList = ["over", "hover", "active", "selected", "scrolled"];
+    if (typeof node !== 'object') {
+        throw new Error('expects dom node')
+    }
+
+    var selector = '';
+
+
+    if (node.nodeName === 'HTML') {
+        return 'html'
+    }
+
+    do {
+        var elSelector = '';
+        if (node.nodeName === 'HTML' | node.nodeName === '#document') {
+            selector += elSelector;
+            break;
+        }
+
+        if (node.id) {
+            elSelector += ' #' + node.id
+            selector += elSelector;
+            break;
+        }
+
+        if (node.nodeName === 'BODY') {
+            elSelector += ' ' + 'body'
+            selector += elSelector;
+            continue;
+        }
+
+        // refactor me *dying cough*
+        if (node.className) {
+            elSelector += ' ' + node.nodeName.toLowerCase() + (node.className.trim().split(/\s+/).map(function (x) {
+                if (ignoreClassList.indexOf(x) === -1) {
+                    return '.' + x.replace('.', '\\\\.');
+                }
+                return '';
+            }).join(''));
+
+            if (node.parentNode.childNodes.length > 1 && node.parentNode.querySelectorAll(elSelector).length > 1) {
+                elSelector += ':nth-child(' + getdata.getNth(node) + ')';
+            }
+        } else {
+            elSelector += ' ' + node.nodeName.toLowerCase();
+            if (node.parentNode.childNodes.length > 1 && node.parentNode.querySelectorAll(elSelector).length > 1) {
+                elSelector += ':nth-child(' + getdata.getNth(node) + ')';
+            }
+        }
+
+        selector += elSelector;
+
+    } while (node = node.parentNode)
+
+    selector = selector.split(' ').reverse().join(' ')
+    return selector || null
+}
+
+function getNth(node) {
+    var startNode = node
+
+    var i = 1
+    while (node = node.previousElementSibling) {
+        ++i
+    }
+    return i
+}
